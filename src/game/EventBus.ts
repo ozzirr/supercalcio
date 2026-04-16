ccba4002a9e8adaa90855f153235fec04ca0718d
Andrea Rizzo <ioandrearizzo@gmail.com>
2026-04-16 16:11:14 +0200

    Update EventBus.ts

--- src/game/EventBus.ts
+++ src/game/EventBus.ts
@@ -1,4 +1,31 @@
-import * as Phaser from "phaser";
+// Lightweight event bus that doesn't depend on Phaser (avoids SSR window issues)
+type Listener = (...args: any[]) => void;
 
-// Used to emit events between React components and Phaser scenes
-export const EventBus = new Phaser.Events.EventEmitter();
+class SimpleEventEmitter {
+  private listeners: Map<string, Listener[]> = new Map();
+
+  on(event: string, fn: Listener) {
+    if (!this.listeners.has(event)) this.listeners.set(event, []);
+    this.listeners.get(event)!.push(fn);
+    return this;
+  }
+
+  off(event: string, fn: Listener) {
+    const fns = this.listeners.get(event);
+    if (fns) this.listeners.set(event, fns.filter(f => f !== fn));
+    return this;
+  }
+
+  emit(event: string, ...args: any[]) {
+    this.listeners.get(event)?.forEach(fn => fn(...args));
+    return this;
+  }
+
+  removeAllListeners(event?: string) {
+    if (event) this.listeners.delete(event);
+    else this.listeners.clear();
+    return this;
+  }
+}
+
+export const EventBus = new SimpleEventEmitter();
